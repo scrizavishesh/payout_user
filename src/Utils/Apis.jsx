2ae20136ccea467356379c47018f2cd6c24f4c17
Vishesh Tomer <vishesh@scriza.in>
2024-10-22 11:54:19 +0530

    first commit

--- src/Utils/Apis.jsx
+++ src/Utils/Apis.jsx
@@ -5,7 +5,7 @@ export const API_URL = "https://auth2.upicollect.com";
 var bearerToken = `Token d21cb51ed49203f081debb4e490684018b6adccc`
 
 
-export const createFund = async ( orderId, recId, id, amount) => {
+export const createFund = async (orderId, recId, id, amount) => {
   axios.defaults.headers.common["Authorization"] = "";
   var response = await axios.get(`${API_URL}/api/payments/create?order_id=${orderId}&receipt_id=${recId}&id=${id}&amount=${amount}`,);
   if (response) {
@@ -17,7 +17,7 @@ export const createFund = async ( orderId, recId, id, amount) => {
 
 export const createOrder = async (requestData, hmac) => {
   axios.defaults.headers.common["Authorization"] = bearerToken;
-  axios.defaults.headers.common["Sign"] = hmac; 
-  const response = await axios.post(`${API_URL}/api/orders/`, requestData);
+  axios.defaults.headers.common["Sign"] = hmac;
+  const response = await axios.post(`${API_URL}/api/payout/create/`, requestData);
   return response;
 };
