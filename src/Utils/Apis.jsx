c296c26e0f1963bd8269460ee21672af0d4d0faa
scrizavishesh <139861699+scrizavishesh@users.noreply.github.com>
2025-03-11 17:57:13 +0530

    Update Apis.jsx

--- src/Utils/Apis.jsx
+++ src/Utils/Apis.jsx
@@ -2,11 +2,11 @@ import axios from "axios";
 
 export const API_URL = "https://auth2.upicollect.com";
 
-var bearerToken = `Token d21cb51ed49203f081debb4e490684018b6adccc`
+// var bearerToken = `Token d21cb51ed49203f081debb4e490684018b6adccc`
 
 
 export const createFund = async (orderId, recId, id, amount) => {
-  axios.defaults.headers.common["Authorization"] = "";
+  // axios.defaults.headers.common["Authorization"] = "";
   var response = await axios.get(`${API_URL}/api/payments/create?order_id=${orderId}&receipt_id=${recId}&id=${id}&amount=${amount}`,);
   if (response) {
     return response;
@@ -16,7 +16,7 @@ export const createFund = async (orderId, recId, id, amount) => {
 }
 
 export const createOrder = async (requestData, hmac) => {
-  // axios.defaults.headers.common["Authorization"] = bearerToken;
+  // axios.defaults.headers.common["Authorization"] = "";
   axios.defaults.headers.common["Sign"] = hmac;
   const response = await axios.post(`${API_URL}/api/payout/create/`, requestData);
   return response;
